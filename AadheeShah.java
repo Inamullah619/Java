faf85b2613a4200e120d0258897a3fd5e78aa4ba
Inamullah619 <inamullahsixonenine@gmail.com>
2024-08-19 12:40:31 +0500

    Calculator

---
+++ AadheeShah.java
@@ -0,0 +1,22 @@
+public class AadheeShah {
+
+    // public static void main(String[] args) {
+    //     String name = "https://google.com";
+    //     System.out.println(name.lastIndexOf("."));
+    // }
+    public static void main(String[] args) {
+        String domain = "https://www.google.com";
+        int dot = domain.lastIndexOf(".");
+        String lastWord = domain.substring(dot + 1);
+
+        System.out.println(lastWord);
+
+        if (domain.equals("com")) {
+            System.out.println("This is a .com domain");
+        } else if (domain.equals("pk")){
+            System.out.println("This is a .pk domain");
+        } else {
+            System.out.println("This is not a .com or .pk domain");
+        }
+    }
+}

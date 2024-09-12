cab8e157ca01182829a032f83272e68721790ed6
Inam ullah <152864264+Inamullah619@users.noreply.github.com>
2024-09-12 14:41:04 +0000

    Star Pattern 12 Sep

--- star.java
+++ star.java
@@ -1,10 +1,25 @@
 public class star {
 
     public static void main(String[] args) {
-        int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.println("*");
+        int row = 5;
+
+
+       for (int i = 1; i <= row; i++) {
+        for (int j = i; j < row; j++) {
+            System.out.print(" ");
+        }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = row; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
             }
             System.out.println();
         }
